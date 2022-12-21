86b730f5dd5ef77edfed6d3a5e4716041a619f3c
Amyx000 <armankazi111@gmail.com>
2022-12-21 15:00:41 +0530

    update

--- Frontend/src/Component/Food/Food.js
+++ Frontend/src/Component/Food/Food.js
@@ -28,12 +28,12 @@ function Food() {
       const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/getfood`, { withCredentials: true })
       dispatch(getallfoods(res.data))
       Setfood(res.data)
-      if(search){
+      // if(search){
         Setshowfood(res.data.filter((item) => {
-          return item.foodname.match(new RegExp(search, "i")) || item.restaurant.match(new RegExp(search, "i"))
+          return item.foodname.match(new RegExp(search, "i")).slice(0, pagination.pageSize) || item.restaurant.match(new RegExp(search, "i")).slice(0, pagination.pageSize)
         }))
-      }
-      Setshowfood([...res.data.slice(0, pagination.pageSize)])
+      // }
+      // Setshowfood([...res.data.slice(0, pagination.pageSize)])
       Setloader(false)
     } catch (error) {
       console.log(error)
